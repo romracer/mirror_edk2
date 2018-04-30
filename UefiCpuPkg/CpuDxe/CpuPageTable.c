d767099285a35b80a7b53651eeeebe8abd4b205a
Nicholas Sherlock <n.sherlock@gmail.com>
2018-04-30 12:59:13 +1200

    Revert readonly pagetables introduced by 2ac1730 and 147fd35
    (cherry picked from commit d16b0bf52e3957561d2857b80aee22417ad7e9f6)

--- UefiCpuPkg/CpuDxe/CpuPageTable.c
+++ UefiCpuPkg/CpuDxe/CpuPageTable.c
@@ -946,7 +946,6 @@ InitializePageTablePool (
   )
 {
   VOID                      *Buffer;
-  BOOLEAN                   IsModified;
 
   //
   // Always reserve at least PAGE_TABLE_POOL_UNIT_PAGES, including one page for
@@ -986,13 +985,12 @@ InitializePageTablePool (
     NULL,
     (PHYSICAL_ADDRESS)(UINTN)Buffer,
     EFI_PAGES_TO_SIZE (PoolPages),
-    EFI_MEMORY_RO,
+    0,
     PageActionSet,
     AllocatePageTableMemory,
     NULL,
-    &IsModified
+    NULL
     );
-  ASSERT (IsModified == TRUE);
 
   return TRUE;
 }
@@ -1062,9 +1060,17 @@ InitializePageTableLib (
   if (CurrentPagingContext.ContextData.X64.PageTableBase != 0 &&
       (CurrentPagingContext.ContextData.Ia32.Attributes &
        PAGE_TABLE_LIB_PAGING_CONTEXT_IA32_X64_ATTRIBUTES_PAE) != 0) {
-    DisableReadOnlyPageWriteProtect ();
+
+    BOOLEAN IsWpEnabled;
+
+    IsWpEnabled = IsReadOnlyPageWriteProtected ();
+    if (IsWpEnabled) {
+      DisableReadOnlyPageWriteProtect ();
+    }
     InitializePageTablePool (1);
-    EnableReadOnlyPageWriteProtect ();
+    if (IsWpEnabled) {
+      EnableReadOnlyPageWriteProtect ();
+    }
   }
 
   DEBUG ((DEBUG_INFO, "CurrentPagingContext:\n", CurrentPagingContext.MachineType));
